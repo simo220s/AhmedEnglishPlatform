0567fe6a8b612752fce2256fa9dff17b5908c8d8
Samer Adel Ahmed Ibrahim <81111741+simo220s@users.noreply.github.com>
2026-01-11 03:32:22 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "@emran-alhaddad/saudi-riyal-font/index.css";
-import "./index.css";
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
